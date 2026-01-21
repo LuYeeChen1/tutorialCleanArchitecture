8ed2b12c919c19a612e5dc7a8f51d1067d66299c
LuYeeChen <luyeechen@03gmail.com>
2026-01-21 09:51:51 +0800

    initial tutorial

--- src/main/java/com/java/tutorial/clean_structure/controller/HelloController.java
+++ src/main/java/com/java/tutorial/clean_structure/controller/HelloController.java
@@ -2,8 +2,16 @@ package com.java.tutorial.clean_structure.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * @RestController: 告诉 Spring 这是一个 Web 接口类
+ */
 @RestController
 public class HelloController {
+
+    /**
+     * @GetMapping("/hello"): 绑定 URL 路径
+     * 当你在浏览器输入 localhost:8080/hello 时，会触发这个方法
+     */
     @GetMapping("/hello")
     public String sayhello(){
         return "Hello, Spring Boot 4.0!";
